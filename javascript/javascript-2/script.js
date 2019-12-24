1e9434733daaadb565bb2a1b9e3b2b6aea8c9f20
diatomiccoder <chudak_93@live.com>
2019-12-25 00:12:56 +0500

    fix + add usage example

--- javascript/javascript-2/script.js
+++ javascript/javascript-2/script.js
@@ -1,1 +1,25 @@
-const promiseReduce = (asyncFunctions, reduce, initValue) => Promise.all(asyncFunctions).then(values => values.reduce(reduce, initValue));
+const promiseReduce = (asyncFunctions, reduce, initValue) =>
+    asyncFunctions.reduce(
+        async (memo, curFunc) => reduce(await memo, await curFunc()),
+        initValue
+    );
+
+const fn1 = () => {
+    console.log("fn1");
+    return Promise.resolve(1);
+};
+
+const fn2 = () =>
+    new Promise(resolve => {
+        console.log("fn2");
+        setTimeout(() => resolve(2), 1000);
+    });
+
+promiseReduce(
+    [fn1, fn2],
+    (memo, value) => {
+        console.log("reduce");
+        return memo * value;
+    },
+    1
+).then(console.log);
