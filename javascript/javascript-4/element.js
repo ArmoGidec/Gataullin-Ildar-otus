7aaba770084720fe63282e3a4999509b6816b9b1
diatomiccoder <chudak_93@live.com>
2020-01-30 17:54:07 +0500

    change 'element' code

--- javascript/javascript-4/element.js
+++ javascript/javascript-4/element.js
@@ -1,12 +1,38 @@
-import { html, render } from 'https://unpkg.com/lit-html?module';
-
-const myLeaf = options => {
-    let leafs = (options.items || []).map(myLeaf);
-
-    return html`${options.id}</br>${leafs}`;
+class BlockElement extends HTMLElement {
+    constructor() {
+        super();
+        this.style.display = 'block';
+    }
 }
 
-const myTree = treeObj => myLeaf(treeObj);
+class MyTree extends BlockElement {
+    constructor(treeObj) {
+        super();
+
+        let leaf = new MyLeaf(treeObj);
+
+        this.innerHTML = 'My Tree:';
+        this.appendChild(leaf);
+    }
+}
+
+customElements.define('my-tree', MyTree);
+
+class MyLeaf extends BlockElement {
+    constructor(options) {
+        super();
+
+        this.innerHTML = `My Leaf: ${options.id}`;
+        this.style.paddingLeft = '13px';
+        if (options.items) {
+            for (let item of options.items) {
+                this.appendChild(new MyLeaf(item));
+            }
+        }
+    }
+}
+
+customElements.define('my-leaf', MyLeaf);
 
 const treeOptions = {
     id: 1,
@@ -26,4 +52,6 @@ const treeOptions = {
     ]
 }
 
-render(myTree(treeOptions), document.querySelector('#element'));
+let myTree = new MyTree(treeOptions);
+
+document.querySelector('#app').appendChild(myTree);
