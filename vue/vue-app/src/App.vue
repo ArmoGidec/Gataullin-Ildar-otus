17b08cbd91e3bbe9e888a48ad275088be7f46685
diatomiccoder <chudak_93@live.com>
2020-04-10 00:15:45 +0500

    layout changes

--- vue/vue-app/src/App.vue
+++ vue/vue-app/src/App.vue
@@ -1,6 +1,16 @@
 <template>
     <div id="app">
-        <SettingsPage />
+        <div class="container py-3">
+            <div class="row">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-4 offset-md-2 offset-lg-3 offset-xl-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <SettingsPage />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
