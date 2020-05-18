5a73803cf2780764768c4133493e31309f8d5c5d
diatomiccoder <chudak_93@live.com>
2020-05-19 02:08:03 +0500

    refactor add-word service

--- angular/angular-app/src/app/add-word.service.ts
+++ angular/angular-app/src/app/add-word.service.ts
@@ -2,27 +2,33 @@ import { Injectable } from '@angular/core';
 import { from } from 'rxjs';
 import { mergeMap } from 'rxjs/operators';
 
-import { TranslateService } from './translate.service';
-import { StorageService } from './storage.service';
+import TranslateService from './translate.service';
+import TranslationsStorageService, { ITranslationObj } from './translationsStorage.service';
 
 @Injectable({
     providedIn: 'root',
 })
-export class AddWordService {
+class AddWordService {
     constructor(
         private translateService: TranslateService,
-        private storageService: StorageService
+        private translationsStorageService: TranslationsStorageService
     ) {}
 
     add = (text: string) => {
         const words = text.split(' ').filter((word) => word.length > 0);
-        from(words)
-            .pipe(
-                mergeMap(
-                    this.translateService.translate,
-                    (word, translated) => ({ word, translation: translated[0] })
-                )
+        const $translation = from(words).pipe(
+            mergeMap(
+                (word) => this.translateService.translate(word),
+                (word, translated) => ({ word, translation: translated[0] })
             )
-            .subscribe(this.storageService.add);
-    }
+        );
+
+        $translation.subscribe((translationObj: ITranslationObj) =>
+            this.translationsStorageService.add(translationObj)
+        );
+
+        return $translation;
+    };
 }
+
+export default AddWordService;
