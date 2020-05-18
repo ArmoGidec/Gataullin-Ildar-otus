9c8a195f99954453002942ac0b8fae3fcf2f6cde
diatomiccoder <chudak_93@live.com>
2020-05-19 02:03:14 +0500

    refactor translate service

--- angular/angular-app/src/app/translate.service.ts
+++ angular/angular-app/src/app/translate.service.ts
@@ -7,7 +7,7 @@ const API_KEY =
     'trnsl.1.1.20200427T185317Z.a3d851b73bd6e006.c4875e300eab125d1e0ed13d8b9455bc06f342ab';
 const API_URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
 
-interface ITranslation {
+export interface ITranslation {
     code: 200 | 401 | 402 | 404 | 413 | 422 | 501;
     text: string[];
     lang: string;
@@ -16,11 +16,11 @@ interface ITranslation {
 @Injectable({
     providedIn: 'root',
 })
-export class TranslateService {
+export default class TranslateService {
     constructor(private http: HttpClient) {}
 
-    translate = (text: string): Observable<string[]> =>
-        this.http
+    translate(text: string): Observable<string[]> {
+        return this.http
             .get<ITranslation>(API_URL, {
                 params: {
                     key: API_KEY,
@@ -29,4 +29,5 @@ export class TranslateService {
                 },
             })
             .pipe(map(({ text }) => text));
+    }
 }
